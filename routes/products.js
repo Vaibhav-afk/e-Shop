e140ddeb37495c77b645cdd9f19a0de0ca98fc88
Vaibhav-afk <vbhvka@gmail.com>
2021-05-23 00:34:02 +0530

    find & add products

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { Product } = require("../models/product");
+const { Category } = require("../models/category");
 
 //promises(error handling) can be made in both ways by using {then, catch,etc} or using async methods and await keyword
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  const productList = await Product.find().select("name image -_id"); //select method helps us to display specific things like here we are displaying name and image without _id
 
   if (!productList) {
     res.status(500).json({
@@ -14,23 +15,45 @@ router.get(`/`, async (req, res) => {
   res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
+router.get(`/:productId`, async (req, res) => {
+  const id = req.params.productId;
+  const product = await Product.findById(id);
+
+  if (!product) {
+    res.status(404).json({
+      success: false,
+      message: `product not found, check the id`,
+    });
+  }
+  res.send(product);
+});
+
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) {
+    return res.status(404).send(`invalid Category`);
+  }
   const product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     quantity: req.body.quantity,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
+  const newProduct = await product.save();
+  if (!newProduct) {
+    res.status(404).json({
+      success: false,
+      message: `failed to add product`,
     });
+  }
+  res.send(product);
 });
 
 module.exports = router;
