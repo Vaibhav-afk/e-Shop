c829cc68b767ae115bcd83bd35bc338dea44d246
Vaibhav-afk <vbhvka@gmail.com>
2021-05-29 12:39:55 +0530

    order & orderItems get/post order populated to get detailed order info

--- routes/orders.js
+++ routes/orders.js
@@ -1,16 +1,66 @@
 const express = require("express");
 const router = express.Router();
+const { OrderItem } = require("../models/order-item");
 const { Order } = require("../models/order");
 
 //promises(error handling) can be made in both ways by using {then, catch,etc} or using async methods and await keyword
 router.get(`/`, async (req, res) => {
-  const orderList = await Order.find();
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
   if (!orderList) {
     res.status(500).json({
       success: false,
     });
   }
   res.send(orderList);
+});
+
+router.get(`/:orderId`, async (req, res) => {
+  const order = await Order.findById(req.params.orderId)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    })
+    .sort({ dateOrdered: -1 });
+  if (!order) {
+    res.status(500).json({
+      success: false,
+    });
+  }
+  res.send(order);
+});
+
+router.post("/", async (req, res) => {
+  const orderItemsIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+      newOrderItem = await newOrderItem.save();
+      return newOrderItem._id;
+    })
+  );
+  const orderItemsIdsResolved = await orderItemsIds;
+  let order = new Order({
+    orderItems: orderItemsIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+  });
+  order = await order.save();
+  if (!order) {
+    return res.status(400).send("the order cannot be placed!");
+  }
+  res.send(order);
 });
 
 module.exports = router;
