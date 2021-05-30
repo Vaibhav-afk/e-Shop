608753a2419527c3e979e2d201b9a83141f2abe9
Vaibhav-afk <vbhvka@gmail.com>
2021-05-30 09:34:59 +0530

    orderCount, get User Orders & cal totalPrice, TotalSales

--- routes/orders.js
+++ routes/orders.js
@@ -44,6 +44,20 @@ router.post("/", async (req, res) => {
     })
   );
   const orderItemsIdsResolved = await orderItemsIds;
+
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
   let order = new Order({
     orderItems: orderItemsIdsResolved,
     shippingAddress1: req.body.shippingAddress1,
@@ -53,7 +67,7 @@ router.post("/", async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrice: req.body.totalPrice,
+    totalPrice: totalPrice,
     user: req.body.user,
   });
   order = await order.save();
@@ -108,6 +122,40 @@ router.delete("/:orderId", (req, res) => {
         error: err,
       });
     });
+});
+
+router.get("/get/totalsales", async (req, res) => {
+  const totalSales = await Order.aggregate([
+    { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
+  ]);
+  if (!totalSales) {
+    return res.status(404).send("The order sales cannot be generated.");
+  }
+  res.send({ totalSales: totalSales.pop().totalsales });
+});
+
+router.get("/get/count", async (req, res) => {
+  const orderCount = await Order.countDocuments((count) => count);
+  if (!orderCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    orderCount: orderCount,
+  });
+});
+
+router.get("/get/userorders/:userId", async (req, res) => {
+  const userorderList = await Order.find({ user: req.params.userId })
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    })
+    .sort({ dateOrdered: -1 });
+
+  if (!userorderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userorderList);
 });
 
 module.exports = router;
